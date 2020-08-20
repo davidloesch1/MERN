5d317b1463b9b20ac4705f77aec378d3327b7046
David <davidloesch1@gmail.com>
2020-08-20 13:00:23 -0700

    backend apis are initially set

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const items = require('./routes/api/items')
+require("dotenv").config();
 
-const app = express()
-
+//initialize app
+const app = express();
 
 // Bodyparser middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+//DB configuration
+const db = require("./config/keys").mongoURI;
+
+//Set mongoose settings
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
+//connect to mongo using mongoose
+mongoose
+  .connect(db)
+  .then(() => console.log("mongo is connected"))
+  .catch((err) => console.log(err));
+
+//Routes
+app.use('/api/items', items)
+
+
+const port = process.env.PORT | 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
