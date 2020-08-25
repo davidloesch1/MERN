b4d17c6164145bd499353ba582f63bae20f0f4ed
David <davidloesch1@gmail.com>
2020-08-25 16:42:40 -0700

    initial commit again

--- vast-castle-03863/server.js
+++ vast-castle-03863/server.js
@@ -40,4 +40,4 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT | 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(proces.env.PORT, () => console.log(`Server started on port ${port}`));
