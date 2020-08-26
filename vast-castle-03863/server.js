a09b527047904d6d8414b16b466c19340dc9c533
David <davidloesch1@gmail.com>
2020-08-25 17:00:03 -0700

    corrections

--- vast-castle-03863/server.js
+++ vast-castle-03863/server.js
@@ -34,12 +34,7 @@ app
     let result = "App is running";
     res.send(result);
   })
-  .listen(app.get("port"), function () {
-    console.log(
-      "App is running, server is listening on port ",
-      app.get("port")
-    );
-  });
+
 
 //Serve Static assests if in production
 if (process.env.NODE_ENV === "production") {
